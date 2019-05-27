73ba9c5d75287d3cb98f77cbfd142b94226a5b24
Calin Dale <citdale@gmail.com>
2019-05-27 13:05:29 +1200

    Controller created
    Code for controlling player is now in its Controller, player pawn is now just a pawn with a controller

--- Scenes/pawn/Pawn.gd
+++ Scenes/pawn/Pawn.gd
@@ -2,17 +2,22 @@ extends Node2D
 
 class_name Pawn
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 enum COLLISION_LAYERS {NONE = 0, NORMAL}
-
 export(COLLISION_LAYERS) var collision = COLLISION_LAYERS.NORMAL
+
+onready var _grid: TileMap = get_parent()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func check_direction(direction: Vector2) -> void:
+	var target_position: Vector2 = _grid.request_move(self, direction)
+	if target_position:
+		move_to(target_position)
+
+func move_to(target_position) -> void:
+	position = target_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- Scenes/player/Controller.gd
+++ Scenes/player/Controller.gd
@@ -1,13 +1,13 @@
-extends "res://Scenes/pawn/Pawn.gd"
+extends Node
 
-onready var grid: TileMap = get_parent()
+onready var _pawn: Pawn = get_parent()
 onready var _move_pause: Timer = get_node("MovePause")
 
 var direction :Vector2 = Vector2()
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+func _ready():
+	pass # Replace with function body.
 
 func _unhandled_input(event: InputEvent) -> void:
 	move_in_direction( get_input_direction())
@@ -19,18 +19,13 @@ func move_in_direction(input_direction: Vector2) -> void:
 		direction = input_direction
 		if _move_pause.is_stopped():
 			_move_pause.start()
-			var target_position: Vector2 = grid.request_move(self, input_direction)
-			if target_position:
-				move_to(target_position)
+			_pawn.check_direction(direction)
 
 func get_input_direction() -> Vector2:
 	return Vector2(
 			int(Input.is_action_pressed("move_east")) - int(Input.is_action_pressed("move_west")),
 			int(Input.is_action_pressed("move_south")) - int(Input.is_action_pressed("move_north"))
 	)
-
-func move_to(target_position) -> void:
-	position = target_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
