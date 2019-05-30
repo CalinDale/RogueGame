1d322910dd983726970d1cc284ac6024c7e4db48
Calin Dale <citdale@gmail.com>
2019-05-30 12:50:20 +1200

    Pathfinding progress
    now draws path along tilemap, avoiding wall tiles.

--- scripts/ControllerManager.gd
+++ scripts/ControllerManager.gd
@@ -4,6 +4,9 @@ extends Node
 const GRID_PATH: NodePath = NodePath("../Grid")
 const START_CONTROLLER_PATH: NodePath = NodePath("../Grid/Player/Controller")
 
+onready var nav_2d: Navigation2D = get_parent()
+onready var line_2d: Line2D = $Line2D
+
 var controller: Node
 
 # Called when the node enters the scene tree for the first time.
@@ -21,6 +24,11 @@ func _move_controller_to_pawn(target_pawn: Pawn):
 	controller.queue_free()
 	controller = new_controller
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("secondary_click"):
+		var new_path = nav_2d.get_simple_path(controller.global_position, $"../Grid".get_global_mouse_position())
+		line_2d.points = new_path
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
